4fc9165b0a2691824a971af4ea6cd487b173c8e6
Damien Vesper <ldamienvesper@gmail.com>
2021-08-31 13:42:05 -0500

    chore: cleanup

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,33 +1,35 @@
-// yes
 const texts = [
-    "sussy imposter among us",
-    "i saw you vented",
-    "i was rdmed in among us darkrp",
-    "thats kinda sus",
-    "make sure to hide the fact you are sus",
+    `sussy imposter among us`,
+    `i saw you vented`,
+    `i was rdmed in among us darkrp`,
+    `thats kinda sus`,
+    `make sure to hide the fact you are sus`,
 ];
-let count = 0;
-let index = 0;
-let curText = 0;
-let letter = 0;
 
-function type() {
-    if (count === texts.length) {
-        count = 0;
-    }
-    curText = texts[count];
-    letter = curText.slice(0, ++index);
+let curText = ``;
+let curLetter = ``;
 
-    document.querySelector(".sus").textContent = letter;
-    if (letter.length === curText.length) {
-        count++;
-        index = 0;
-        setTimeout(type, 1000)
+let textIndex = 0;
+let letterIndex = 0;
+
+const type = () => {
+    if (textIndex === texts.length) textIndex = 0;
+
+    curText = texts[textIndex];
+    curLetter = curText.slice(0, ++letterIndex);
+
+    document.querySelector(`.sus`).textContent = curLetter;
+
+    if (letterIndex === curText.length) {
+        textIndex++;
+        letterIndex = 0;
+        setTimeout(type, 1000);
         return;
     }
-    setTimeout(type, 50)
-}
+
+    setTimeout(type, 50);
+};
 
 window.onload = () => {
     type();
-}
+};
